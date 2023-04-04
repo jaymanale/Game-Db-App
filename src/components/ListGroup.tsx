f5382ae123e75e671a82e84b7dd8c5458aa10082
jaymanale <jaymanale29@gmail.com>
2023-04-04 22:38:36 +0530

    Rendering list dynamically using map method

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,15 @@
 function ListGroup() {
+  let states = ["Maharashtra", "Delhi", "UP", "MP", "Goa"];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {states.map((state) => (
+          <li key={state} className="list-group-item">
+            {state}
+          </li>
+        ))}
       </ul>
     </>
   );
