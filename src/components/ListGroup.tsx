287e497c210ef70303071dfb026da8c304ade75a
jaymanale <jaymanale29@gmail.com>
2023-04-04 22:50:06 +0530

    Conditional rendering of data different approach

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,23 @@
 function ListGroup() {
   let states = ["Maharashtra", "Delhi", "UP", "MP", "Goa"];
+  states = [];
 
+  // Using Constant value
+  let checkIfStateEmptyMessage =
+    states.length === 0 ? <p>No State found.</p> : null;
+
+  // Using function
+  function checkIfStateEmptyMessageFunction() {
+    return states.length === 0 ? <p>No State found.</p> : null;
+  }
   return (
     <>
       <h1>List</h1>
+      {/* {checkIfStateEmptyMessage} */}
+      {/* {checkIfStateEmptyMessageFunction()} */}
+
+      {/* Following is much concise approach for conditional rendering. */}
+      {states.length == 0 && <p>No State found.</p>}
       <ul className="list-group">
         {states.map((state) => (
           <li key={state} className="list-group-item">
