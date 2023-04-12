8258f2ad5388128e9d461084ef7070ee4559c951
jaymanale <jaymanale29@gmail.com>
2023-04-12 19:20:55 +0530

    update cart details based on id

--- src/components/UpdateStateExercise.tsx
+++ src/components/UpdateStateExercise.tsx
@@ -13,6 +13,14 @@ const UpdateStateExercise = () => {
     toppings: ["paneer"],
   });
 
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "product 1", quantity: 1 },
+      { id: 2, title: "product 2", quantity: 1 },
+    ],
+  });
+
   const handleUpdate = () => {
     setGames({ ...games, player: { ...games.player, name: "Bob" } });
   };
@@ -21,10 +29,20 @@ const UpdateStateExercise = () => {
     setPizza({ ...pizza, toppings: [...pizza.toppings, "pineapple"] });
   };
 
+  const handleCart = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
+  };
+
   return (
     <>
       <button onClick={handleUpdate}>update game</button>
       <button onClick={handlePizza}>Update Pizza</button>
+      <button onClick={handleCart}>Update Pizza</button>
     </>
   );
 };
