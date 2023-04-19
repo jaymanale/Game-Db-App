aafca5a64a515c971135ffcba12602b94c546335
jaymanale <jaymanale29@gmail.com>
2023-04-19 20:51:42 +0530

    fetching details using useEffect based on Category

--- src/components/useEffects/ProductsLists.tsx
+++ src/components/useEffects/ProductsLists.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 
-const ProductsLists = () => {
+interface Props {
+  category: string;
+}
+
+const ProductsLists = ({ category }: Props) => {
   let [products, setProducts] = useState([]);
 
   useEffect(() => {
-    console.log("fetching products...");
-  }, []); // If there is no dependency then to render only once we pass empty array []
+    console.log("fetching products in " + category);
+  }, [category]); // If there is no dependency then to render only once we pass empty array []
   return <div>ProductsLists</div>;
 };
 
--- src/components/useEffects/UseEffectsMain.tsx
+++ src/components/useEffects/UseEffectsMain.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import UseEffectExplain from "./UseEffectExplain";
 import ProductsLists from "./ProductsLists";
 
 const UseEffectsMain = () => {
+  const [category, setCategory] = useState("");
   return (
     <>
-      <UseEffectExplain />
-      <ProductsLists />
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">CLothing</option>
+        <option value="Household">Household</option>
+      </select>
+      {category}
+      <ProductsLists category={category} />
     </>
   );
 };
